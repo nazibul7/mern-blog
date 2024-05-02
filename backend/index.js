d448fabf024443c1bf8f058e8fa5b4c5cb0dbbe0
Sk Nazibul Hossain <sknazibul5986@gmail.com>
2024-05-02 22:52:05 +0530

    "connected to the databse"

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,19 @@
 import express from "express"
+import mongoose from "mongoose";
+import dotenv from "dotenv"
 
-const app=express()
+dotenv.config()
+mongoose.connect(process.env.MONFODB_URI+'/mern-blog')
+    .then(() => {
+        console.log('MongoDB is connected');
+    })
+    .catch((err) => {
+        console.log("MongoDB error", err);
+    })
 
-app.listen(3000,()=>{
+
+const app = express()
+
+app.listen(3000, () => {
     console.log('Server is running at port 3000');
 })
