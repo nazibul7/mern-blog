068ad433e86b3fa650976a694150041fe98ec87b
Sk Nazibul Hossain <sknazibul5986@gmail.com>
2024-05-03 15:23:07 +0530

    add a middleware and function to handle errors

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -1,19 +1,20 @@
 import { User } from "../models/user.model.js"
 import bcryptjs from "bcryptjs"
+import { errorHandler } from "../utils/apiError.js"
 
-export const signup = async (req, res) => {
+export const signup = async (req, res, next) => {
     try {
         const { username, email, password } = req.body
         if (!username || !email || !password || username == "" || email == "" || password == "") {
-            return res.status(400).json({ message: "All fields are required" })
+            next(errorHandler(400, "All fields are required"))
         }
         const hashPassword = bcryptjs.hashSync(password, 10)
         const newUser = await User.create({
-            username, email, password:hashPassword
+            username, email, password: hashPassword
         })
         await newUser.save()
         res.json({ message: "Signup successful" })
     } catch (error) {
-        res.send(error.message)
+        next(error)
     }
 }
--- backend/index.js
+++ backend/index.js
@@ -5,7 +5,7 @@ import userRoutes from "./routes/user.route.js";
 import authRoutes from "./routes/auth.route.js"
 
 dotenv.config()
-mongoose.connect(process.env.MONFODB_URI+'/mern-blog')
+mongoose.connect(process.env.MONFODB_URI + '/mern-blog')
     .then(() => {
         console.log('MongoDB is connected');
     })
@@ -21,5 +21,15 @@ app.listen(3000, () => {
 })
 
 app.use(express.json())
-app.use("/api/user",userRoutes)
-app.use('/api/auth',authRoutes)
+app.use("/api/user", userRoutes)
+app.use('/api/auth', authRoutes)
+
+app.use((err, req, res, next) => {
+    const statusCode = err.statusCode || 500
+    const message = err.message || "Something went wrong"
+    res.status(statusCode).json({
+        success: false,
+        statusCode,
+        message
+    })
+})
