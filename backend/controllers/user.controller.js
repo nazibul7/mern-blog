6f428de1f6b25721ee2cbfea342428c43ea06ab2
Sk Nazibul Hossain <sknazibul5986@gmail.com>
2024-05-18 00:42:12 +0530

    add update user api route

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -1,3 +1,53 @@
+import { errorHandler } from "../utils/apiError.js"
+import bcryptjs from "bcryptjs"
+import { User } from "../models/user.model.js"
+
 export const test = (req, res) => {
     res.send("Api is working")
 }
+
+export const updateUser = async (req, res, next) => {
+    if (req.user.userId != req.params.userId) {
+        return next(errorHandler(403, 'You are not allowed to update user'))
+    }
+    const { password, username, email, profilePicture } = req.body
+    if(!username && !password && !email && !profilePicture){
+        return next(errorHandler(400,"No valid fields to update"))
+    }
+    if (password) {
+        if (req.body.password.length < 6) {
+            return next(errorHandler(400, "password must be at least 6 characters"))
+        }
+        req.body.password = bcryptjs.hashSync(req.body.password, 10)
+    }
+    if (username) {
+        if (username.length < 7 || username.length > 20) {
+            return next(errorHandler(400, "username must be between 7 and 20 charcters"))
+        }
+        if (username.includes(' ')) {
+            return next(errorHandler(400, "Username cannot contain spaces"))
+        }
+        if (username != username.toLowerCase()) {
+            return next(errorHandler(400, "Username must not conatin uppercase"))
+        }
+        if (!username.match(/^[a-zA-Z0-9]+$/)) {
+            return next(errorHandler(400, "Username only conatin letters and numbers"))
+        }
+    }
+    try {
+        const updatedUser = await User.findByIdAndUpdate(req.params.userId,
+            {
+                $set: {
+                    username: req.body.username,
+                    email: req.body.email,
+                    profilePicture: req.body.profilePicture,
+                    password: req.body.password,
+                }
+            }, { new: true }
+        ).select('-password')
+        res.status(200).json(updatedUser)
+    } catch (error) {
+        return next(error)
+    }
+}
+
--- backend/routes/user.route.js
+++ backend/routes/user.route.js
@@ -1,8 +1,10 @@
 import express, { Router } from "express"
-import { test } from "../controllers/user.controller.js"
+import { test, updateUser } from "../controllers/user.controller.js"
+import { verifyToken } from "../utils/varifyUser.js"
 
 const router = express.Router()
 
 router.route('/test').get(test)
+router.put('/update/:userId', verifyToken, updateUser)
 
 export default router
