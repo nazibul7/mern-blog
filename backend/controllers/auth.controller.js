65b53cc648002c4cb1c7eab3f5e2722f9e7f71a0
Sk Nazibul Hossain <sknazibul5986@gmail.com>
2024-05-19 16:34:42 +0530

    add admin functionality to the user

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -29,14 +29,14 @@ export const signin = async (req, res, next) => {
     try {
         const validUser = await User.findOne({ email })
         if (!validUser) {
-           return next(errorHandler(404, "User not found"))
+            return next(errorHandler(404, "User not found"))
         }
         const checkPassword = bcryptjs.compareSync(password, validUser.password)
         if (!checkPassword) {
             return next(errorHandler(400, "Invalid password"))
         }
         const token = jwt.sign(
-            { userId: validUser._id, username: validUser.username },
+            { userId: validUser._id, username: validUser.username, isAdmin: validUser.isAdmin },
             process.env.JWT_SECRET
         )
         const { password: pass, ...rest } = validUser._doc
@@ -55,7 +55,7 @@ export const google = async (req, res, next) => {
     try {
         const user = await User.findOne({ email })
         if (user) {
-            const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET)
+            const token = jwt.sign({ id: user._id, isAdmin: user.isAdmin }, process.env.JWT_SECRET)
             const { password, ...rest } = user._doc
             res.status(200).cookie('accessToken', token, {
                 httpOnly: true
@@ -70,7 +70,7 @@ export const google = async (req, res, next) => {
                 password: hashPassword,
                 profilePicture: googlePhotoUrl
             })
-            const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET)
+            const token = jwt.sign({ id: newUser._id, isAdmin: (await newUser).isAdmin }, process.env.JWT_SECRET)
             const { password, ...rest } = newUser
             res.status(200).cookie('accessToken', token, {
                 httpOnly: true
--- backend/models/user.model.js
+++ backend/models/user.model.js
@@ -15,9 +15,13 @@ const userSchema = new mongoose.Schema({
         type: String,
         required: true
     },
-    profilePicture:{
-        type:String,
-        default:'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSKSzhiQWKfmkIEgC4nK9ShYjiWz6-DJgxeNyMoBLLabBo1e5kzMq_TE9_rFzSJpPow264&usqp=CAU'
+    profilePicture: {
+        type: String,
+        default: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSKSzhiQWKfmkIEgC4nK9ShYjiWz6-DJgxeNyMoBLLabBo1e5kzMq_TE9_rFzSJpPow264&usqp=CAU'
+    },
+    isAdmin: {
+        type: Boolean,
+        default: false
     }
 }, { timestamps: true })
 
