57963d00a6820e0a55a6a3651e09b2b107f4f591
Sk Nazibul Hossain <sknazibul5986@gmail.com>
2024-06-13 22:54:47 +0530

    add correct logic likeComment controller to prevent from minus

--- backend/controllers/comment.controller.js
+++ backend/controllers/comment.controller.js
@@ -46,7 +46,9 @@ export const likeComment = async (req, res, next) => {
             likeComment.likes.push(req.user.userId)
         }
         else {
-            likeComment.numberOfLikes -= 1
+            if(likeComment.numberOfLikes>0){
+                likeComment.numberOfLikes -= 1
+            }
             likeComment.likes.splice(userIndex, 1)
         }
         await likeComment.save()
@@ -57,7 +59,6 @@ export const likeComment = async (req, res, next) => {
 }
 
 // The method comment.save() is used to save the changes made to the comment document in the database. Here's why this is necessary and why Comment.save() wouldn't work in this context:
-
 // Saving Changes to a Document:
 // When you modify a Mongoose document (like updating the numberOfLikes or modifying the likes array), those changes are only made in memory. To persist these changes to the database, you need to call the save() method on the document instance. This method ensures that the updated state of the document is written to the database.
 
@@ -65,3 +66,27 @@ export const likeComment = async (req, res, next) => {
 
 // comment.save(): This is an instance method called on a specific document instance. It saves the current state of that particular document to the database.
 // Comment.save(): This would imply calling save() on the Model itself, which is not valid. The save() method is meant to be called on document instances, not on the Model constructor. The Model (in this case, Comment) provides static methods to create, read, update, and delete documents, but it doesn't have a save() method applicable to the Model as a whole.
+
+export const editComment = async (req, res, next) => {
+    console.log(req.body);
+    console.log(req.params);
+    console.log(req.user);
+    try {
+        const comment = await Comment.findById(req.params.commentId)
+        if (!comment) {
+            return next(errorHandler(404, "Comment not found"))
+        }
+        if (comment.userId != req.user.userId && !req.user.isAdmin) {
+            return next(errorHandler(403, "You are not allowed to to edit this comment"))
+        }
+        const editedComment = await Comment.findByIdAndUpdate(req.params.commentId,
+            {
+                $set: {
+                    commentContent: req.body.content
+                }
+            }, { new: true })
+        res.status(200).json(editedComment)
+    } catch (error) {
+        next(error)
+    }
+}
