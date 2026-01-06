11a689f838c9c3710d5b06c21ae0bd6bc5268afa
Sk Nazibul Hossain <sknazibul5986@gmail.com>
2026-01-06 20:24:22 +0530

    add branded logo in signup & signin pages

--- client/src/pages/Signup.jsx
+++ client/src/pages/Signup.jsx
@@ -2,6 +2,7 @@ import { Alert, Button, Label, Spinner, TextInput } from 'flowbite-react'
 import { useState } from 'react'
 import { Link, useNavigate } from 'react-router-dom'
 import OAuth from '../components/OAuth'
+import BlogForgeLogo from '../components/BlogForgeLogo';
 
 const Signup = () => {
   const navigate = useNavigate();
@@ -47,8 +48,9 @@ const Signup = () => {
           
           {/* Left side - Brand */}
           <div className='flex-1 md:flex flex-col justify-center mb-8 md:mb-0 hidden'>
-            <Link to={'/'} className='text-3xl sm:text-4xl font-bold'>
-              <span className='px-2 py-1 bg-gradient-to-r from-indigo-600 via-purple-600 to-pink-600 bg-clip-text text-transparent hover:from-indigo-500 hover:via-purple-500 hover:to-pink-500 transition-all'>
+            <Link to={'/'} className='flex items-center tracking-tight text-3xl sm:text-4xl font-bold'>
+              <BlogForgeLogo size='xl' showText={false} />
+              <span className='px-2 py-1 ml-[-15px] bg-gradient-to-r from-indigo-600 via-purple-600 to-pink-600 bg-clip-text text-transparent hover:from-indigo-500 hover:via-purple-500 hover:to-pink-500 transition-all'>
                 BlogForge
               </span>
             </Link>
